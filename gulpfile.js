85be34156a0b79c43272a0cbcb1fc479064f48a4
nishtha-sharma1 <sharmanish22@gmail.com>
2019-04-28 13:21:14 -0400

    working on gulp dependencies

---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+
+function copy() {
+    return gulp.src([
+      'app/*.html',
+     // 'app/**/*.jpg',
+    ])
+    .pipe(gulp.dest('build'));
+  }
+
+gulp.task('copy', copy);
+
+function serve() {
+    return browserSync.init({
+      server: 'build',
+      open: false,
+      port: 3000
+    });
+  }
+
+  function reload(done) {
+    browserSync.reload();
+    done();
+  }
+
+function processJs() {
+    return gulp.src('app/js/*.js')
+    .pipe(babel({
+        presets: ['env']
+    }))
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('build/js'));
+  }
+
+gulp.task('processJs', processJs);
+
+function watch() {
+    gulp.watch('app/js/*.js', gulp.series(processJs, reload));
+    gulp.watch('app/sass/*.scss', gulp.series(compileSass, reload));
+  }
+
+gulp.task('watch', watch);
+
+  function compileSass() {
+    return gulp.src('app/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('build/sass'));
+  }
+  
+gulp.task('sass', compileSass);
+
+gulp.task('buildAndServe', gulp.series(copy, serve));
+  
+gulp.task('buildAndServe', 
+  gulp.series(
+     copy,
+     processJs,
+     compileSass,
+     gulp.parallel(serve,watch),
+     )
+);
