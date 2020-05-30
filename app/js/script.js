ef463dfc78651d8426b2fa3c546b7bf317aeff72
nishtha-sharma1 <sharmanish22@gmail.com>
2020-05-30 12:55:39 -0400

    adding changes

--- app/js/script.js
+++ app/js/script.js
@@ -9,29 +9,84 @@ document.addEventListener("DOMContentLoaded", function() {
 
   menuText.addEventListener('click', () => {
      toggleMenu.classList.toggle('js-toggle-menu--open');
+     if (toggleMenu.classList.contains('js-toggle-menu--open')) {
+      menuText.innerHTML = 'Close';
+     } else {
+      menuText.innerHTML = 'Menu';
+     } 
   });
 
-  const menuLink = document.querySelectorAll('.menu__link');
-  if (window.location.pathname !== "/") {
-    menuText.classList.add('medium-dark');
-    menuLink.forEach((i) => {
-      i.classList.add('medium-dark');
-    })
+  // Debounce function for scroll
+  // From: https://medium.com/@TCAS3/debounce-deep-dive-javascript-es6-e6f8d983b7a1
+  // TODO: Learn this function. 
+  const debounce = (fn, time) => {
+    let timeout;
+  
+    return function() {
+      const functionCall = () => fn.apply(this, arguments);
+      
+      clearTimeout(timeout);
+      timeout = setTimeout(functionCall, time);
+    }
   }
 
+  const w = window.innerWidth;
+
+  window.addEventListener('resize', debounce((e)=>{
+    if (w > 768) {
+      toggleMenu.classList.remove('js-toggle-menu--open');
+      menuText.innerHTML = 'Menu';
+    }
+  } ,1000));
+
+  const contactFormButton = document.querySelector('.js-contact');
+  const contactForm = document.querySelector('.form__wrapper');
+  const closeButton = document.querySelector('.js-close');
+
+  contactFormButton.addEventListener('click', () => {
+    contactForm.classList.add('form__wrapper--open');
+    contactForm.addEventListener('click', (e) => {
+      const place = e.target.closest('.form__contact');
+      if (!place) {
+        contactForm.classList.remove('form__wrapper--open');
+      }
+    })
+  });
+
+  closeButton.addEventListener('click', () => {
+    contactForm.classList.remove('form__wrapper--open');
+  });
+
+  // Change text color by page
+  const menuLink = document.querySelectorAll('.menu__link');
+  const homeLink = document.querySelector('.home__link');
+  const logoText = document.querySelector('.logo-text');
+
+  if (window.location.pathname === "/") {
+    menuText.classList.add('white-menu-text');
+    logoText.classList.add('logo-text-opposite');
+    menuLink.forEach((i) => {
+      i.classList.add('white');
+    });
+    homeLink.classList.add('home-link-hide');
+  }
+  
   // Show More Function Button 
   const showMoreButton = document.querySelector('.js-show-more');
-  const showMoreSection = document.querySelectorAll('.show-more-section');
+  
+  if (showMoreButton) {
+    const showMoreSection = document.querySelectorAll('.show-more-section');
 
-  showMoreButton.addEventListener('click', () => {
-    showMoreButton.classList.toggle('show-more--open');
-    showMoreSection.forEach((i) => {
-      i.classList.toggle('show-more-section--open');
+    showMoreButton.addEventListener('click', () => {
+      showMoreButton.classList.toggle('show-more--open');
+      showMoreSection.forEach((i) => {
+        i.classList.toggle('show-more-section--open');
+      });
+      if (showMoreButton.classList.contains('show-more--open')) {
+        showMoreButton.innerHTML = 'See Less';
+      } else {
+        showMoreButton.innerHTML = 'See More';
+      }
     });
-    if (showMoreButton.classList.contains('show-more--open')) {
-      showMoreButton.innerHTML = 'See Less';
-    } else {
-      showMoreButton.innerHTML = 'See More';
-    }
-  });
+  }
 });
